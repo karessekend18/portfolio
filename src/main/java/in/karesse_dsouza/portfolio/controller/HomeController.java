adbae2a92f415b331973ead24e476f8f1982be9a
Karesse DSouza <kadsouza+cisco@cisco.com>
2025-06-30 15:59:11 +0530

    Initial commit

---
+++ src/main/java/in/karesse_dsouza/portfolio/controller/HomeController.java
@@ -0,0 +1,35 @@
+package in.karesse_dsouza.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "", "/home"})
+    public String showHomePage(Model model) {
+        model.addAttribute("title", "Home");
+        return "master";
+    }
+
+    @GetMapping("/projects")
+    public String showProjectsPage(Model model) {
+        model.addAttribute("title", "Projects");
+        return "master";
+    }
+
+    @GetMapping("/resume")
+    public String showResumePage(Model model) {
+        model.addAttribute("title", "Resume");
+        return "master";
+    }
+
+    @GetMapping("/contact")
+    public String showContactPage(Model model) {
+        model.addAttribute("title", "Contact");
+        return "master";
+    }
+
+
+}
